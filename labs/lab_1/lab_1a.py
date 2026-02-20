4acd3f0a8f224b31233aede51ca544f62394bf4d
Jacob <jacobmean2008@gmail.com>
2026-02-19 19:44:22 -0500

    Added introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Jacob" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi guys!, I am Jacob and I like to play the trombone!")
 
 if __name__ == "__main__":
     main()
